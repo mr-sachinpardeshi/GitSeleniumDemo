481bcf100ebbd4bf98647d3988ed0a6fa8b90281
Sachin <mr.sachinpardeshi@gmail.com>
2025-02-05 13:55:32 +0530

    ~Architect committed in development mode

--- SeleniumRoboustIndustryStandardFramework/src/test/java/baseClass/GitSeleniumDemo.java
+++ SeleniumRoboustIndustryStandardFramework/src/test/java/baseClass/GitSeleniumDemo.java
@@ -1,5 +1,16 @@
 package baseClass;
+
+import org.testng.annotations.Test;
+
 //This class is added by GitSeleniumDemo for Git Stuff
 public class GitSeleniumDemo {
 	//This line is updated by GitSeleniumDemo
+	
+	
+	//develop branch updating
+	@Test
+	public void develop() {
+		System.out.println("develop branch changes updated");
+	}
 }
+
